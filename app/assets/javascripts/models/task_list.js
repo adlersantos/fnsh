040287480d3a7bfbdaa4ecda342ac84da29b997a
Adler Santos <adlersantos712@gmail.com>
2013-09-03 20:05:12 -0700

    added views and sortable for tasks

--- app/assets/javascripts/models/task_list.js
+++ app/assets/javascripts/models/task_list.js
@@ -1,3 +1,9 @@
 BC.Models.TaskList = Backbone.Model.extend({
+  parse: function (data) {
+    var tasks = new BC.Collections.Tasks(data.tasks);
+    data.tasks = tasks;
+
+    return data;
+  }
 
 });
