97e5f17a88c0c2716cc5d7e7dffe26bb61ef4d60
Adler Santos <adlersantos712@gmail.com>
2013-09-08 02:26:03 -0700

    implement task creation

--- app/assets/javascripts/models/task.js
+++ app/assets/javascripts/models/task.js
@@ -3,6 +3,13 @@ BC.Models.Task = Backbone.Model.extend({
     var taskList = new BC.Models.TaskList(data.task_list);
     data.task_list = taskList;
 
+    data.project_id = data.project.id
+
     return data;
+  },
+
+  urlRoot: function () {
+    return '/projects/' + this.get('project_id') + '/task_lists/'
+             + this.get('task_list_id') + '/tasks/';
   }
 });
