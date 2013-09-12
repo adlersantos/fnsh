19bca1139fe0cc670bdb2dc5704d7488ae2c939d
Adler Santos <adlersantos712@gmail.com>
2013-09-12 16:54:05 -0700

    implement rename inline task

--- app/assets/javascripts/views/tasks/task.js
+++ app/assets/javascripts/views/tasks/task.js
@@ -5,7 +5,17 @@ BC.Views.Task = Backbone.View.extend({
   events: {
     "click .delete-task": "deleteTask",
     "click input.task-checkbox": "toggleTaskCompletion",
-    "click span.task-name": "showTaskDetail"
+    "click span.task-name": function (event) {
+      this.showTaskDetail(event);
+      this.putRenameTaskForm(event);
+    },
+    "click .cancel-inline-rename-task": "cancelRenameTask",
+    "click button.rename-inline-task": "renameTask"
+  },
+
+  cancelRenameTask: function (event) {
+    $(event.currentTarget).parent().toggleClass('hidden');
+    $(event.currentTarget).parent().prev().toggleClass('hidden');
   },
 
   deleteTask: function (event) {
@@ -13,6 +23,20 @@ BC.Views.Task = Backbone.View.extend({
 
     this.model.url = this.model.urlRoot() + this.model.get('id');
     this.model.destroy();
+  },
+
+  putRenameTaskForm: function (event) {
+    $(event.currentTarget).toggleClass('hidden');
+    $(event.currentTarget).next().toggleClass('hidden');
+    $(event.currentTarget).next().find('input').focus();
+  },
+
+  renameTask: function (event) {
+    event.preventDefault();
+
+    var taskData = $(event.currentTarget).parent().serializeJSON();
+    this.model.url = this.model.urlRoot() + this.model.get('id');
+    this.model.save(taskData, {wait: true});
   },
 
   render: function () {
