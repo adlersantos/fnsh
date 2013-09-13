0890ba490b4feb7b026add0fea430c82b62c5ca8
Adler Santos <adlersantos712@gmail.com>
2013-09-13 14:42:06 -0700

    made task detail rename full width

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ActiveRecord::Base
   attr_accessible :author_id, :body, :task_id
 
+  validates :author_id, :body, :task_id, :presence => true
+
   belongs_to :task,
     class_name: "Task",
     foreign_key: :task_id
